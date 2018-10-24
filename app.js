4bc0c346ccfb96e5fc2011c0a327c2c2da6c5fd1
vj <vijayarasan@hasgrid.com>
2018-10-24 12:30:21 +0530

    new commit

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 var express = require('express'),
     fs = require('fs'),
     app = express();
- 
+
 var app = express();
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
- 
- 
-app.get('/', function(req, res) {
-    res.send('Hello from NodeJS  at '+ new Date() + 'Running Successfully.' );
+    ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+
+app.get('/', function (req, res) {
+    res.send('Hello from NodeJS  at ' + new Date() + 'Running Successfully.' +
+        '/n' + 'commit working...');
     // res.render('index.html', { pageCountMessage : null});
 });
- 
+
 app.listen(port, ip, function () {
-    console.log( "Listening on " + ip + ", port " + 8080 )
-  });
- 
+    console.log("Listening on " + ip + ", port " + 8080)
+});
+
 module.exports = app;
