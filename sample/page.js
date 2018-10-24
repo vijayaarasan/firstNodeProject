cb658942a1119452d3ab1ea812a846bc73dee2ea
vj <vijayarasan@hasgrid.com>
2018-10-24 17:35:08 +0530

    .

--- sample/page.js
+++ sample/page.js
@@ -9,3 +9,12 @@ module.exports.one = function (req, res) {
 module.exports.two = function (req, res) {
     res.send('succesfully got api two');
 };
+
+module.exports.three = function (req, res) {
+    let name = req.param('name');
+    if (!name) {
+        res.send('wrong query');
+    } else {
+        res.send('Hello' + name + '!');
+    }
+};
