d2838915954d9cacfabee9cc93102de4c008f91e
vj <vijayarasan@hasgrid.com>
2018-10-24 17:32:35 +0530

    api  post three

--- route/route.js
+++ route/route.js
@@ -3,4 +3,5 @@ var sampRoute = express.Router();
 var samplePage = require('../sample/page');
 sampRoute.get('/smp1', samplePage.one);
 sampRoute.get('/smp2', samplePage.two);
+sampRoute.post('/smp3', samplePage.three);
 module.exports = sampRoute;
